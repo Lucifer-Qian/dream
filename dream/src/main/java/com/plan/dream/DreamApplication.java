05f9f92c16adc68c53be3c2936965924e0e28a75
“Lucifer-Qian” <973361054@qq.com>
2023-11-07 22:19:46 +0800

    11

--- dream/src/main/java/com/plan/dream/DreamApplication.java
+++ dream/src/main/java/com/plan/dream/DreamApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author 97336
+ */
 @SpringBootApplication
 @ComponentScan(value = "com.plan.dream.dao")
 public class DreamApplication {
